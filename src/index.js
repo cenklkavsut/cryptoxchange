0343e36755fd4508967241e76697ff2af84d8a3d
cenklkavsut <cenkkavsut@hotmail.com>
2021-02-11 17:24:24 +0300

    fixed a problem a bit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Root from './components/root';
 //CSS
-import './assets/scss/boostrap/bootstrap.css'
-import './assets/scss/itrade.css'
+//import './assets/scss/boostrap/bootstrap.css'
+//import './assets/scss/itrade.css'
 import 'react-circular-progressbar/dist/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(<Root />, document.getElementById('root'));
